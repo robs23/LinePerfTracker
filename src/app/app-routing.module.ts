48b17dd369d2358add94a91f60a2f4e7b376e6a8
Robert Roszak <robert.roszak@outlook.com>
2020-08-10 11:27:58 +0200

    Trying to get router to named outlet working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MachinesComponent } from './machines/machines.component';
+import { MachineComponent } from './machine/machine.component';
 import { LinePerformanceComponent } from './line-performance/line-performance.component';
 
 const routes: Routes = [
   { path: 'machines', component: MachinesComponent},
-  { path: 'lineperformance', component: LinePerformanceComponent, children:[
-    { path: 'machine/:id', component: MachinesComponent, outlet: 'main'}
-  ]},
+  { path: 'lineperformance', component: LinePerformanceComponent},
+  { path: 'machine/:id', component: MachineComponent, outlet: 'main'},
   { path: '', redirectTo: '/lineperformance', pathMatch: 'full'}
 ];
 
