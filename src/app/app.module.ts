dcf7020ef4b30705d239a5977a4bb3dd5dd0d6dd
Robert Roszak <robert.roszak@outlook.com>
2022-01-13 13:36:00 +0100

    planned-components-grid implemented yet still doesn't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import {VgControlsModule} from '@videogular/ngx-videogular/controls';
 import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
 import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
 import { PlannedComponentsGridComponent } from './planned-components-grid/planned-components-grid.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
    declarations: [													
@@ -63,7 +64,8 @@ import { PlannedComponentsGridComponent } from './planned-components-grid/planne
       VgCoreModule,
       VgControlsModule,
       VgOverlayPlayModule,
-      VgBufferingModule
+      VgBufferingModule,
+      AgGridModule.withComponents([])
    ],
    providers: [ Settings, SpinnerService ],
    bootstrap: [
--- src/app/planned-components-grid/planned-components-grid.component.ts
+++ src/app/planned-components-grid/planned-components-grid.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PlannedComponent } from '../interfaces/planned-component';
 import { PlannedComponentsService } from '../services/planned-components.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ColDef } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-planned-components-grid',
@@ -9,8 +11,89 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./planned-components-grid.component.css']
 })
 export class PlannedComponentsGridComponent implements OnInit {
-  PlannedComponents: PlannedComponent[];
-  constructor(private componentService: PlannedComponentsService, private params: ActivatedRoute) { }
+  PlannedComponents: Observable<any[]>;
+  colDefs: ColDef[];
+
+  constructor(private componentService: PlannedComponentsService, private params: ActivatedRoute) {
+    this.colDefs = [
+      { 
+        headerName: 'Czas',
+        field: 'OPERATION_DATE'
+      },
+      {
+        headerName: 'Data',
+        field: 'OPERATION_DAY'
+      },
+      {
+        headerName: 'Tydzień',
+        field: 'OPERATION_WEEK'
+      },
+      {
+        headerName: 'Rok',
+        field: 'OPERATION_YEAR'
+      },
+      { 
+        headerName: 'Nr zmiany',
+        field: 'SHIFT_ID'
+      },
+      {
+        headerName: 'Zmiana',
+        field: 'SHIFT_NAME'
+      },
+      {
+        headerName: 'Maszyna',
+        field: 'MACHINE_NR'
+      },
+      {
+        headerName: 'Operacja',
+        field: 'OPERATION_NR'
+      },
+      { 
+        headerName: 'Obszar',
+        field: 'OPERATION_TYPE_NAME'
+      },
+      {
+        headerName: 'Zlecenie',
+        field: 'ORDER_NR'
+      },
+      {
+        headerName: 'Index',
+        field: 'PRODUCT_NR'
+      },
+      {
+        headerName: 'Nazwa',
+        field: 'PRODUCT_NAME'
+      },
+      { 
+        headerName: 'Grupa',
+        field: 'PROD_TYPE'
+      },
+      {
+        headerName: 'Typ',
+        field: 'SUB_PROD_TYPE'
+      },
+      {
+        headerName: 'Kod',
+        field: 'ORDER_TYPE_CODE'
+      },
+      {
+        headerName: 'Kod2',
+        field: 'ORDER_TYPE_NAME'
+      },
+      {
+        headerName: 'BOM',
+        field: 'BOM_NR'
+      },
+      {
+        headerName: 'Ilość [szt.]',
+        field: 'PRODUCT_QUANTITY'
+      },
+      {
+        headerName: 'Ilość w operacji [szt.]',
+        field: 'PRODUCT_QUANTITY_ALL'
+      }
+    ]
+   }
 
   ngOnInit() {
     let query: string = '';
@@ -26,7 +109,8 @@ export class PlannedComponentsGridComponent implements OnInit {
 
   getPlannedComponents(query?: string): void{
     this.componentService.getPlannedComponents(query).subscribe(response => 
-      { this.PlannedComponents = response;
+      { 
+        this.PlannedComponents = response;
       });
   }
 
