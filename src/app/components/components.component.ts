251d58ee002509dd42b6c632ff82fe738cdf1658
Robert Roszak <robert.roszak@outlook.com>
2022-03-06 15:36:11 +0100

    Implementing changing range for planned components

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -3,12 +3,15 @@ import { Settings } from '../settings';
 import { Subject } from 'rxjs';
 import { UserInteractionService } from '../services/userInteraction.service';
 
+
 @Component({
   selector: 'app-components',
   templateUrl: './components.component.html',
   styleUrls: ['./components.component.scss']
 })
 export class ComponentsComponent implements OnInit {
+
+  
 
   constructor(public settings: Settings, private userInteractionService: UserInteractionService) { }
 
@@ -27,6 +30,10 @@ export class ComponentsComponent implements OnInit {
         this.settings.PlanCoverageByDeliveries = true;
         this.userInteractionService.emitPlanCoverageByDeliveriesClicked(true);
     }
+  }
+
+  changePlanCoverageScope(): void{
+    
   }
 
 }
